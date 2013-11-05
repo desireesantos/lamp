15f250d28973d6bf77532feb279aaee8a8702a23
Desiree Santos <desisant@gmail.com>
2013-11-05 00:55:00 -0200

    Update readme text and add integration with arduino

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-Running
-=======
+Lamp V2
+==============
 
+
+To run:
+-------
+
+> First:
+ Install [node.js] (http://nodejs.org/)
+	
+> Second: 	
 ```bash
 make && make run
 ```
--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 var narf = require('narf');
-// var board = new require('johnny-five').Board();
+var board = new require('johnny-five').Board();
 
-// board.on('ready', function() {
-//   var val = 0;
+board.on('ready', function() {
+  var val = 0;
  
-//   this.pinMode( 13, 1 );
-//   this.digitalWrite( 13, 1 );
+  this.pinMode( 13, 1 );
+  this.digitalWrite( 13, 1 );
   
-//   var self = this;
+  var self = this;
  
-//   var APIFunctions = {
-//     POST : {
-//       ledSwitch : function ( data, callback ){
-//           console.log('DATA: '+ data.url.value);
-//         data.url.value = parseInt( data.url.value, 0 );
-//         if( data.url.value === 1 || data.url.value === 0){
-//             console.log(' BINGO ');
-//           self.digitalWrite( 13, data.url.value );
-//         }
-//         callback( data.url.value );
-//       }
-//     }
-//     };
+  var APIFunctions = {
+    POST : {
+      ledSwitch : function ( data, callback ){
+          console.log('DATA: '+ data.url.value);
+        data.url.value = parseInt( data.url.value, 0 );
+        if( data.url.value === 1 || data.url.value === 0){
+            console.log(' BINGO ');
+          self.digitalWrite( 13, data.url.value );
+        }
+        callback( data.url.value );
+      }
+    }
+    };
 
-//   console.log( narf );
+  console.log( narf );
  
-// var hs = new narf.HttpServer( { port : 8080 } ).start();
-// hs.on( 'port', function( port ){
-//   console.log('Almost there !');
-//   hs.addAPI( { functions : APIFunctions } );
-// } );
+var hs = new narf.HttpServer( { port : 8080 } ).start();
+hs.on( 'port', function( port ){
+  console.log('Almost there !');
+  hs.addAPI( { functions : APIFunctions } );
+} );
   
-// });
+});
 
 narf.pageServer({ 
   port: 8000,
